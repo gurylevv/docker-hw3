ea7be0dcfdd8576c797b70ae60c81e94e1a78c34
Гурылёв Илья <gurylev.crypto@gmail.com>
2026-06-11 16:23:10 +0300

    Добавлены команды structure, clear_data, inside_generator, inside_reporter

--- run.sh
+++ run.sh
@@ -24,6 +24,23 @@ case "$COMMAND" in
         docker run --rm -v "$(pwd)/data:/data" reporter
         ;;
 
+    structure)
+        find . -not -path './.git/*'
+        ;;
+
+    clear_data)
+        rm -f data/*.csv data/*.html
+        echo "Папка data очищена"
+        ;;
+
+    inside_generator)
+        docker run --rm -v "$(pwd)/data:/data" generator ls -la /data
+        ;;
+
+    inside_reporter)
+        docker run --rm -v "$(pwd)/data:/data" reporter ls -la /data
+        ;;
+
     *)
         echo "Неизвестная команда: $COMMAND"
         echo "Доступные команды: build_generator, run_generator, create_local_data"
